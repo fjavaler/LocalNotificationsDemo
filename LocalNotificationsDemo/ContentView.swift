2896d459cffadbe0b3d27579fe3be4b4d235cdce
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-02 16:58:33 -0600

    Initial Commit

---
+++ LocalNotificationsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalNotificationsDemo
+//
+//  Created by Frederick Javalera on 6/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalNotificationsDemo/LocalNotificationsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsDemoApp.swift
+//  LocalNotificationsDemo
+//
+//  Created by Frederick Javalera on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
